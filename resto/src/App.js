8ae0bdf7a17fd1cdb5cbfb03b7d878b8f7b3002c
jiancaballero <jiancaballero@gmail.com>
2022-08-26 19:40:43 +0900

    fix: alignment total amount

--- resto/src/App.js
+++ resto/src/App.js
@@ -876,7 +876,7 @@ function App() {
               </Paper>
               <Divider />
               <Paper elevation={0} sx={{ padding: "1em", marginTop: "auto" }}>
-                <Typography variant="h5" gutterBottom>Total Amount: {state.total}.00</Typography>
+              <Typography sx={{display:"flex", justifyContent: "space-between"}} variant="h5" gutterBottom>Total Amount <Stack>{state.total}.00</Stack></Typography>
                 <Button sx={{width:"100%"}} variant="contained" disableElevation color="secondary" size="large">Checkout</Button>
               </Paper>
             </>
