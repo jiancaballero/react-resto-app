2b78c055b5845379bb67dfb0945831944769f992
jiancaballero <jiancaballero@gmail.com>
2022-08-26 23:12:23 +0900

    fix: ratings alignment styling

--- resto/src/components/Ratings.js
+++ resto/src/components/Ratings.js
@@ -6,7 +6,7 @@ const Ratings = ({ ratings }) => {
     <Stack direction="row" spacing={2}>
 
       <Rating value={ratings} precision={0.5} readOnly size="medium"></Rating>
-      <Typography variant="body2">{ratings}</Typography>
+      <Typography variant="body2" sx={{display:'flex',alignItems:'flex-end'}}>{ratings}</Typography>
     </Stack>
   );
 };
